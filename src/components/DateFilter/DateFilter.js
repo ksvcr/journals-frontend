4ddb225e42b7a76b731b55af9b82aa381a597a1e
Sergey Kuklin <ksv.crowley@gmail.com>
2018-09-18 19:44:10 +0500

    Опции для фильтра

--- src/components/DateFilter/DateFilter.js
+++ src/components/DateFilter/DateFilter.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
 
 import './date-filter.scss';
+import Select from '~/components/Select/Select';
 
 class DateFilter extends Component {
+  get filterOptions() {
+    return [{
+      title: 'Дата создания',
+      value: 'date_public'
+    },
+    {
+      title: 'Дата отправки',
+      value: 'date_sent'
+    },
+    {
+      title: 'Последнее изменение',
+      value: 'last_change'
+    }];
+  }
+
   render() {
     return (
       <div className="date-filter">
-        тут будет фильтр
+        <Select options={ this.filterOptions } onChange={ this.handleChange } className="select_small" />
       </div>
     );
   }
