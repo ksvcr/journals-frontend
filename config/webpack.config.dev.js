46d3161673b5df4223e02e9af1d79c625f6b21c3
Sergey Kuklin <ksv.crowley@gmail.com>
2018-09-26 16:37:14 +0500

    fix webpack config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -155,7 +155,7 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.(css|scss|scss)$/,
+            test: /\.(css|scss|sass)$/,
             use: [
               require.resolve('style-loader'),
               {
