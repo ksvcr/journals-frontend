a917c51f977690ec1346958fb9e9bb05badb0b19
Sergey Kuklin <ksv.crowley@gmail.com>
2018-11-05 22:45:46 +0500

    tests

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+
+import Button from './Button';
+
+describe('Render Button', () => {
+  it('render correctly default Button component', () => {
+    const ButtonComponent = TestRenderer.create(<Button>Кнопка</Button>).toJSON();
+    expect(ButtonComponent).toMatchSnapshot();
+  });
+
+  it('render correctly Button component with link type', () => {
+    const props = {
+      type: 'link'
+    };
+
+    const ButtonComponent = mount(
+      <MemoryRouter>
+        <Button { ...props }>Кнопка</Button>
+      </MemoryRouter>);
+    console.log(ButtonComponent);
+    expect(ButtonComponent.exists('a')).toEqual(
+      true
+    );
+  });
+});
+
