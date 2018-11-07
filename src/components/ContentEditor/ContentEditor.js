20197fb8021db4271d398cfd5f6e23828e281278
Sergey Kuklin <ksv.crowley@gmail.com>
2018-11-07 21:31:20 +0500

    editor tools

--- src/components/ContentEditor/ContentEditor.js
+++ src/components/ContentEditor/ContentEditor.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
 import createToolbarPlugin, { Separator } from 'draft-js-static-toolbar-plugin';
+import createLinkPlugin from 'draft-js-anchor-plugin';
+import createUndoPlugin from 'draft-js-undo-plugin';
 
 import {
   ItalicButton,
@@ -8,49 +10,79 @@ import {
   UnderlineButton
 } from 'draft-js-buttons';
 
-import 'draft-js-static-toolbar-plugin/lib/plugin.css';
-import 'draft-js-alignment-plugin/lib/plugin.css';
+import HeadlinesSelect from '~/components/HeadlinesSelect/HeadlinesSelect'
 
+import 'draft-js-static-toolbar-plugin/lib/plugin.css';
+import 'draft-js-anchor-plugin/lib/plugin.css';
+
+const linkPlugin = createLinkPlugin();
+const { LinkButton } = linkPlugin;
+const undoPlugin = createUndoPlugin();
+const { UndoButton, RedoButton } = undoPlugin;
 const toolbarPlugin = createToolbarPlugin();
 const { Toolbar } = toolbarPlugin;
-const plugins = [toolbarPlugin];
+
+const plugins = [toolbarPlugin, linkPlugin, undoPlugin];
 const text = 'In this editor a toolbar shows up once you select part of the text 1…';
 
 class ContentEditor extends Component {
   state = {
     editorState: createEditorStateWithText(text),
+    isExpanded: false
   };
 
   focus = () => {
     this.editor.focus();
   };
 
-  renderButtons = (externalProps) => (
-    <React.Fragment>
-      <ItalicButton {...externalProps} />
-      <BoldButton {...externalProps} />
-      <UnderlineButton {...externalProps} />
-      <Separator />
-    </React.Fragment>
-  );
+  handleExpand = () => {
+    this.setState(({ isExpanded }) => ({
+      isExpanded: !isExpanded
+    }));
+  };
+
+  renderButtons = (externalProps) => {
+    console.log(externalProps);
+    return (
+      <React.Fragment>
+        <HeadlinesSelect {...externalProps} />
+        <Separator />
+        <BoldButton {...externalProps} />
+        <ItalicButton {...externalProps} />
+        <UnderlineButton {...externalProps} />
+        <Separator />
+        <button type="button" onClick={ this.handleExpand }>+</button>
+
+        { this.state.isExpanded &&
+          <React.Fragment>
+            <LinkButton {...externalProps} />
+            <Separator />
+            <UndoButton />
+            <RedoButton />
+          </React.Fragment>
+        }
+
+      </React.Fragment>
+    )
+  };
 
   handleChange = (editorState) => {
-    console.log('editorState ==>', editorState.toJS());
+    // console.log('editorState ==>', editorState.toJS());
     this.setState({ editorState })
   };
 
   render() {
     return (
       <div onClick={this.focus}>
-        <Toolbar>
-          { this.renderButtons }
-        </Toolbar>
         <Editor
           editorState={this.state.editorState}
           onChange={this.handleChange}
           plugins={ plugins }
           ref={(element) => { this.editor = element; }}
         />
+        <Toolbar>
+          { this.renderButtons }
+        </Toolbar>
       </div>
     );
   }
