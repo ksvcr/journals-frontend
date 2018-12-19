c52f4d5a3bc233820ff00f9d390797317d29c628
Nikita Korshunov <k.nikita@jetstyle.ru>
2018-12-19 13:25:29 +0500

    Add settings route

---
+++ src/containers/AuthorSettings.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class AuthorSettings extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {};
+}
+
+export default connect(
+  mapStateToProps
+)(AuthorSettings);
