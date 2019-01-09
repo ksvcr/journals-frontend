afe5df442b8bc5f4daeef1814a8500cdac3764dc
Журавлев Павел <pzhuravlyov@jetstyle.ru>
2019-01-09 18:00:32 +0500

    выбор размера шрифта

---
+++ src/components/FontSizeTool/FontSizeTool.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import { styles } from '~/services/editorCustomStyler';
+import './font-size-tool.scss';
+
+const DEFAULT_FONT_SIZE = 14;
+
+class FontSizeTool extends Component {
+  get fontSizes() {
+    return [ 12, 14, 16, 18, 20, 22, 24, 26, 28, 30 ];
+  }
+
+  renderFontSizes = () => {
+    return this.fontSizes.map((fontSize) => (
+      <option key={ fontSize } value={ fontSize }> { fontSize } </option>
+    ));
+  };
+
+  handleChangeFontSize = ({ target }) => {
+    const { getEditorState, setEditorState } = this.props;
+    const { value:fontSize } = target;
+    const newState = styles.fontSize.add(getEditorState(), `${ fontSize }px`);
+    setEditorState(newState);
+  }
+
+  render() {
+    return (
+      <div className="font-size-tool">
+        <select className="font-size-tool__select" name="font_size" defaultValue={ DEFAULT_FONT_SIZE } onChange={ this.handleChangeFontSize }>
+          { this.renderFontSizes() }
+        </select>
+      </div>
+    );
+  }
+}
+
+export default FontSizeTool;
