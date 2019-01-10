6df4316364e4d9dee3f13bf8a62af7d3704520b0
Nikita Korshunov <k.nikita@jetstyle.ru>
2019-01-10 16:40:25 +0500

    Add estimate fields in ReviewCreateForm

--- src/components/ReviewCreateForm/ReviewCreateForm.js
+++ src/components/ReviewCreateForm/ReviewCreateForm.js
@@ -6,8 +6,14 @@ import { push } from 'connected-react-router';
 import TextField from '~/components/TextField/TextField';
 import ReqMark from '~/components/ReqMark/ReqMark';
 import Button from '~/components/Button/Button';
+import List from '~/components/List/List';
+import Radio from '~/components/Radio/Radio';
+
+import getEstimateFields from '~/services/getEstimateFields';
 
 import * as validate from '~/utils/validate';
+
+import './review-create-form.scss';
 
 class ReviewCreateForm extends Component {
   componentDidUpdate() {
@@ -15,6 +21,74 @@ class ReviewCreateForm extends Component {
     if (!articleData) {
       push('/');
     }
+  }
+
+  renderEstimateRadio = (value) => (data) => (
+    <div className="review-create-form__estimate__radio">
+      <Field name={ data.id } value={ value } type="radio" component={ Radio } />
+    </div>
+  );
+
+  get estimateProps() {
+    const radioCellStyle = {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    };
+
+    return {
+      data: getEstimateFields(),
+      head: true,
+      cells: [
+        {
+          style: {
+            width: '25%'
+          },
+          head: () => 'Ваше мнение о статье',
+          render: (data) => <p className="review-create-form__estimate__title">{ data.title }</p>
+        },
+        {
+          style: {
+            width: '13%',
+            ...radioCellStyle
+          },
+          head: () => 'Не согласен',
+          render: this.renderEstimateRadio('1')
+        },
+        {
+          style: {
+            width: '20%',
+            ...radioCellStyle
+          },
+          head: () => 'Скорее не согласен',
+          render: this.renderEstimateRadio('2')
+        },
+        {
+          style: {
+            width: '12%',
+            ...radioCellStyle
+          },
+          head: () => 'Нейтрален',
+          render: this.renderEstimateRadio('3')
+        },
+        {
+          style: {
+            width: '20%',
+            ...radioCellStyle
+          },
+          head: () => 'Скорее согласен',
+          render: this.renderEstimateRadio('4')
+        },
+        {
+          style: {
+            width: '10%',
+            ...radioCellStyle
+          },
+          head: () => 'Согласен',
+          render: this.renderEstimateRadio('5')
+        }
+      ]
+    };
   }
 
   render() {
@@ -28,6 +102,9 @@ class ReviewCreateForm extends Component {
           </label>
           <Field name="comment_for_author" id="comment_for_author" component={ TextField } textarea rows={ 20 }
                  placeholder="Введите текст рецензии" />
+        </div>
+        <div className="review-create-form__estimate">
+          <List { ...this.estimateProps } />
         </div>
         <div className="form__field">
           <label htmlFor="comment_for_redactor" className="form__label">
---
+++ src/services/getEstimateFields.js
@@ -0,0 +1,20 @@
+export default function getEstimateFields() {
+  return [
+    {
+      id: 'estimate_actual_problem',
+      title: 'Рассматриваемая в статье научная проблема является актуальной'
+    },
+    {
+      id: 'estimate_sci_style',
+      title: 'Статья профессионально и грамотно написана, изложение соответствует научному стилю'
+    },
+    {
+      id: 'estimate_methods_conformity',
+      title: 'Используемые методы и подходы соответствуют целям и задачам исследования'
+    },
+    {
+      id: 'estimate_sci_novelty',
+      title: 'Статья имеет научную новизну, а результаты значимы'
+    }
+  ];
+}
