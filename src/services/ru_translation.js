19d2f6cebcf52d1953745321ab794f8b6148225a
Sergey Kuklin <ksv.crowley@gmail.com>
2019-02-04 12:04:04 +0500

    Перевод

--- src/services/ru_translation.js
+++ src/services/ru_translation.js
@@ -1,7 +1,17 @@
 const ru_translation = {
   support: 'Поддержка сайта',
   our_journals: 'Наши журналы',
-  publish_article: 'Опубликовать статью'
+  publish_article: 'Опубликовать статью',
+  settings: 'Настройки',
+  my_articles: 'Мои статьи',
+  article_search: 'Поиск статьи',
+  for_journals: 'Для журнала',
+  search_in_title: 'Искать в заголовках',
+  search_everywhere: 'Искать везде',
+  search: 'Поиск',
+  title: 'Заголовок',
+  stage: 'Этап',
+  state: 'Статус'
 };
 
 export default ru_translation;
