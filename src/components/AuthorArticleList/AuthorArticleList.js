24c41915c03a40d6633d0ad1b89bf27ab251ac5a
Sergey <ksv.crowley@gmail.com>
2019-02-20 16:14:48 +0500

    Кавычки

--- src/components/AuthorArticleList/AuthorArticleList.js
+++ src/components/AuthorArticleList/AuthorArticleList.js
@@ -1,80 +1,81 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { push } from 'connected-react-router';
-import { withNamespaces } from 'react-i18next';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+import { withNamespaces } from "react-i18next";
 
-import List from '~/components/List/List';
-import DateFilter from '~/components/DateFilter/DateFilter';
-import PaginateLine from '~/components/PaginateLine/PaginateLine';
-import StatusLabel from '~/components/StatusLabel/StatusLabel';
-import ToolsMenu from '~/components/ToolsMenu/ToolsMenu';
-import Payment from '~/components/Payment/Payment';
+import List from "~/components/List/List";
+import DateFilter from "~/components/DateFilter/DateFilter";
+import PaginateLine from "~/components/PaginateLine/PaginateLine";
+import StatusLabel from "~/components/StatusLabel/StatusLabel";
+import ToolsMenu from "~/components/ToolsMenu/ToolsMenu";
+import Payment from "~/components/Payment/Payment";
 
-import { getArticlesArray } from '~/store/articles/selector';
+import { getArticlesArray } from "~/store/articles/selector";
 
-import * as formatDate from '~/services/formatDate';
-import { getArticleStageTitle } from '~/services/articleStages';
+import * as formatDate from "~/services/formatDate";
+import { getArticleStageTitle } from "~/services/articleStages";
 
 class AuthorArticleList extends Component {
   state = {
     box: null,
-    dateField: 'date_create'
+    dateField: "date_create"
   };
 
   getToolsMenuItems(data) {
     const { locked_by } = data;
-    const {t, userId} = this.props;
+    const { t, userId } = this.props;
     const isLocked = locked_by !== null && locked_by !== userId;
     let items = [];
 
     if (!isLocked) {
       items.push({
-        title: t('edit'),
+        title: t("edit"),
         handler: this.handleEdit
       });
     }
 
-    items = [ ...items,
+    items = [
+      ...items,
       {
-        title: 'Оплатить',
+        title: "Оплатить",
         handler: this.handlePaymentShow
       },
       {
-        title: 'Просмотр',
-        type: 'preview',
-        icon: 'preview',
+        title: "Просмотр",
+        type: "preview",
+        icon: "preview",
         handler: this.handlePreview
       }
     ];
 
     return items;
-  };
+  }
 
   get dateTitle() {
     return {
-      'date_create': 'Создана',
-      'date_send_to_review': 'Отправлена',
-      'last_change': 'Изменена'
+      date_create: "Создана",
+      date_send_to_review: "Отправлена",
+      last_change: "Изменена"
     };
-  };
+  }
 
-  handleSortChange = (ordering) => {
+  handleSortChange = ordering => {
     const { onUpdateRequest } = this.props;
     onUpdateRequest({ ordering });
   };
 
-  handlePaymentShow = (id) => {
+  handlePaymentShow = id => {
     this.setState({
-      box: { id,  type: 'payment' }
+      box: { id, type: "payment" }
     });
   };
 
-  handleEdit = (id) => {
+  handleEdit = id => {
     const { push } = this.props;
     push(`/article/${id}/edit/`);
   };
 
-  handlePreview = (id) => {
+  handlePreview = id => {
     const { push } = this.props;
     push(`/article/${id}`);
   };
@@ -89,7 +90,7 @@ class AuthorArticleList extends Component {
     onUpdateRequest({ filter: range });
   };
 
-  handlePaginateChange = (paginate) => {
+  handlePaginateChange = paginate => {
     const { onUpdateRequest } = this.props;
     onUpdateRequest({ paginate });
   };
@@ -102,57 +103,58 @@ class AuthorArticleList extends Component {
       data: articlesArray,
       onSortChange: this.handleSortChange,
       head: true,
-      menuTooltip: (data) => <ToolsMenu id={ data.id } items={ this.getToolsMenuItems(data) } />,
+      menuTooltip: data => (
+        <ToolsMenu id={data.id} items={this.getToolsMenuItems(data)} />
+      ),
       box: this.renderBox,
       cells: [
         {
           style: {
-            width: '50%'
+            width: "50%"
           },
           isMain: true,
-          head: () => t('title'),
-          render: (data) =>
-            data.title || 'Название статьи не указано'
+          head: () => t("title"),
+          render: data => data.title || "Название статьи не указано"
         },
         {
           style: {
-            width: '12%'
+            width: "12%"
           },
           sort: dateField,
           head: () => this.dateTitle[dateField],
-          headToolTip: () =>
-            <DateFilter field={ dateField }
-                        onChange={ this.handleDateFilterChange } />,
-          render: (data) =>
-            formatDate.toString(data[dateField])
+          headToolTip: () => (
+            <DateFilter
+              field={dateField}
+              onChange={this.handleDateFilterChange}
+            />
+          ),
+          render: data => formatDate.toString(data[dateField])
         },
         {
           style: {
-            width: '13%'
+            width: "13%"
           },
-          sort: 'stage_article',
-          head: () => t('stage'),
-          render: (data) =>
-            getArticleStageTitle(data.stage_article)
+          sort: "stage_article",
+          head: () => t("stage"),
+          render: data => getArticleStageTitle(data.stage_article)
         },
         {
           style: {
-            width: '20%'
+            width: "20%"
           },
-          sort: 'state_article',
-          head: () => t('state'),
-          render: (data) =>
-            <StatusLabel status={ data.state_article } />
+          sort: "state_article",
+          head: () => t("state"),
+          render: data => <StatusLabel status={data.state_article} />
         }
       ]
     };
   }
 
-  renderBox = (data) => {
+  renderBox = data => {
     const { box } = this.state;
     if (box && box.id === data.id) {
-      if (box.type === 'payment') {
-        return <Payment onClose={ this.handlePaymentClose } />;
+      if (box.type === "payment") {
+        return <Payment onClose={this.handlePaymentClose} />;
       }
     }
 
@@ -164,12 +166,16 @@ class AuthorArticleList extends Component {
     return (
       <div className="author-article-list">
         <div className="author-article-list__holder">
-          <List { ...this.listProps } />
+          <List {...this.listProps} />
         </div>
 
-        { total > 0 &&
-          <PaginateLine onChange={ this.handlePaginateChange } total={ total } { ...paginate } />
-        }
+        {total > 0 && (
+          <PaginateLine
+            onChange={this.handlePaginateChange}
+            total={total}
+            {...paginate}
+          />
+        )}
       </div>
     );
   }
@@ -181,7 +187,8 @@ function mapStateToProps(state) {
   return {
     articlesArray: getArticlesArray(state),
     userId: user.data.id,
-    total, paginate
+    total,
+    paginate
   };
 }
 
@@ -192,5 +199,6 @@ const mapDispatchToProps = {
 AuthorArticleList = withNamespaces()(AuthorArticleList);
 
 export default connect(
-  mapStateToProps, mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(AuthorArticleList);
