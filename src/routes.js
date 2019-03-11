36c29fddc87196f0feb13e74410194d8aba3ca8e
Sergey <ksv.crowley@gmail.com>
2019-03-11 15:45:44 +0500

    Deadline field

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { hot } from 'react-hot-loader';
 
 import Page from '~/containers/Page';
 import RoleAccess from '~/containers/RoleAccess';
-import Articles from '~/containers/Articles';
-import ArticlePublish from '~/containers/ArticlePublish';
-import ArticlePreview from '~/containers/ArticlePreview';
-import ArticleTranslate from '~/containers/ArticleTranslate';
-import AuthorSettings from '~/containers/AuthorSettings';
-import ArticlesForReview from '~/containers/ArticlesForReview';
-import ReviewCreate from '~/containers/ReviewCreate';
-import Discounts from '~/containers/Discounts';
 import NotFound from '~/containers/NotFound';
-import RedactorUsers from '~/containers/RedactorUsers';
-import ReviewPreview from '~/containers/ReviewPreview';
+
+const Articles = lazy(() => import('~/containers/Articles'));
+const ArticleTranslate = lazy(() => import('~/containers/ArticleTranslate'));
+const AuthorSettings = lazy(() => import('~/containers/AuthorSettings'));
+const ArticlesForReview = lazy(() => import('~/containers/ArticlesForReview'));
+const ArticlePublish = lazy(() => import('~/containers/ArticlePublish'));
+const ArticlePreview = lazy(() => import('~/containers/ArticlePreview'));
+const ReviewPreview = lazy(() => import('~/containers/ReviewPreview'));
+const Discounts = lazy(() => import('~/containers/Discounts'));
+const RedactorUsers = lazy(() => import('~/containers/RedactorUsers'));
+const ReviewCreate = lazy(() => import('~/containers/ReviewCreate'));
 
 const ArticlePublishWithAccess = RoleAccess(ArticlePublish, ['AUTHOR', 'REVIEWER']);
 const ArticleEditWithAccess = RoleAccess(ArticlePublish, ['AUTHOR', 'REVIEWER', 'REDACTOR']);
@@ -27,22 +28,24 @@ const AuthorSettingsWithAccess = RoleAccess(AuthorSettings, ['REDACTOR']);
 
 const routes = () => (
   <Page>
-    <Switch>
-      <Route exact path="/" component={ Articles } />
-      <Route exact path="/article" component={ ArticlePublishWithAccess } />
-      <Route exact path="/article/:articleId" component={ ArticlePreview } />
-      <Route path="/article/:articleId/edit" component={ ArticleEditWithAccess } />
-      <Route exact path="/article/:articleId/review" component={ ReviewCreateWithAccess } />
-      <Route path="/article/:articleId/review/:reviewId" component={ ReviewPreview } />
-      <Route path="/article/:articleId/translate" component={ ArticleTranslateWithAccess } />
-      <Route path="/article/:articleId/correct" component={ ArticleCorrectWithAccess }/>
-      <Route path="/articles-for-review" component={ ArticlesForReviewWithAccess } />
-      <Route path="/users" component={ RedactorUsersWithAccess } />
-      <Route path="/settings/:userId" component={ AuthorSettingsWithAccess } />
-      <Route path="/settings" component={ AuthorSettings } />
-      <Route path="/discounts" component={ Discounts } />
-      <Route component={ NotFound } />
-    </Switch>
+    <Suspense fallback={ <div>Загрузка...</div> }>
+      <Switch>
+        <Route exact path="/" component={ props => <Articles { ...props } /> } />
+        <Route exact path="/article" component={ ArticlePublishWithAccess } />
+        <Route exact path="/article/:articleId" component={ props => <ArticlePreview { ...props } /> } />
+        <Route path="/article/:articleId/edit" component={ ArticleEditWithAccess } />
+        <Route exact path="/article/:articleId/review" component={ ReviewCreateWithAccess } />
+        <Route path="/article/:articleId/review/:reviewId" component={ props => <ReviewPreview { ...props } /> } />
+        <Route path="/article/:articleId/translate" component={ ArticleTranslateWithAccess } />
+        <Route path="/article/:articleId/correct" component={ ArticleCorrectWithAccess }/>
+        <Route path="/articles-for-review" component={ ArticlesForReviewWithAccess } />
+        <Route path="/users" component={ RedactorUsersWithAccess } />
+        <Route path="/settings/:userId" component={ AuthorSettingsWithAccess } />
+        <Route path="/settings" component={ props => <AuthorSettings { ...props } /> } />
+        <Route path="/discounts" component={ props => <Discounts { ...props } /> } />
+        <Route component={ props => <NotFound { ...props } /> } />
+      </Switch>
+    </Suspense>
   </Page>
 );
 
